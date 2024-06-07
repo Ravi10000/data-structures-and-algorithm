3f949122a0a0ce7733c3f2e6d006dfff6af65dcb
@raviSince2k <ravisince2k@gmail.com>
2024-06-07 14:33:25 +0530

    implement optimal solution for count digits

--- 1.count-digits.js
+++ 1.count-digits.js
@@ -1,5 +1,26 @@
+// solution 1
+// function countDigits(n) {
+//     return n ? String(n).length : 0;
+// }
+
+
+// solution 2
+// function countDigits(n) {
+//     let count = 0;
+//     console.log({ n });
+//     while (n > 0) {
+//         count++;
+//         n = parseInt(n / 10);
+//         console.log({ n });
+//         if (n < 10)
+//             return count + 1;
+//     }
+//     return count;
+// }
+
+// optimal solution 
 function countDigits(n) {
-    return n ? String(n).length : 0;
+    return parseInt(Math.log10(n) + 1)
 }
 
-console.log({ count: countDigits(123123123) });
+console.log({ count: countDigits(122342343) });
