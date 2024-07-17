4d09c349392a3ef5875d4665e775d57e499c67b9
@raviSince2k <ravisince2k@gmail.com>
2024-07-17 15:53:23 +0530

    restructure max consicutive sum

--- array_questions/max_sum.js
+++ array_questions/max_sum.js
@@ -1,18 +1,17 @@
 // return 0, if all numbers in the array is negative;
 // return 0, if array length is 0;
 function findMaxSum(array) {
-    const arrayLength = array.length;
-    let maxSum = array[0] ?? 0;
-    let tempSum = array[0];
-    for (let i = 1; i < arrayLength; i++) {
-        tempSum += array[i];
+    let maxSum = 0;
+    let tempSum = 0;
+    for (let n of array) {
+        tempSum += n;
         if (tempSum > maxSum) maxSum = tempSum;
-        if (tempSum <= 0) tempSum = 0;
+        if (tempSum < 0) tempSum = 0;
     }
-    return maxSum < 0 ? 0 : maxSum;
+    return maxSum;
 }
 
-console.log(findMaxSum([])); // 0
-console.log(findMaxSum([-2, -3, -3, -6, -4, -5])); // 0
+// console.log(findMaxSum([])); // 0
+// console.log(findMaxSum([-2, -3, -3, -6, -4, -5])); // 0
 console.log(findMaxSum([2, 3, -3, -1, 2, 3, 4, 5, -6, -4, 2, 5, -8])); // 15
 //                      2, 5,  2,  1, 3, 6,10,15                       _
